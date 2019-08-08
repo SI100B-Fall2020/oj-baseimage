80c6b71c06fd0b40f5cb65fd4dabcb8fd9b3cfb8
llk89 <27812632+llk89@users.noreply.github.com>
2019-08-09 00:05:07 +0800

    Update grade.py

--- grading/grade.py
+++ grading/grade.py
@@ -16,7 +16,18 @@ with Judging() as e:
 			# Please consult /run.py on what this err can be
 			err=int(t.read().strip(" \n\r"))
 			t.close()
+			# This will declare a testcase that worth 1 point, but the student's program errored for some reason.
+			# Unless you made a drastical change like change to use Java, you aren't too likely to change this part.
 			e.add_errored_testcase(err,1)
 		else:
 			passed = compare_file("/test/{}".format(i), "/judge/tests/output/{}".format(i))
-			e.add_boolean_testcase(passed)
+			# If your test case is simple yes/no, use add_boolean_testcase
+			# This will declare a testcase that worth 1 point.
+			# For demonstration purpose, we don't use add_boolean_testcase here, but it is 
+			# 100% valid to use it here.
+			# e.add_boolean_testcase(passed)
+			
+			# If you want to make it more than 1 point, or want to award partial success, 
+			# e.g. give 1 point for a 2 pts testcase,
+			# use e.add_scored_testcase
+			e.add_scored_testcase(10 if passed else 0, 10)
