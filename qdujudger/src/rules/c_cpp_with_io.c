527806c48cc42dda3601ffc1fe87043fb8ef9ad1
llk89 <27812632+llk89@users.noreply.github.com>
2019-12-12 18:10:52 +0800

    Update c_cpp_with_io.c

--- qdujudger/src/rules/c_cpp_with_io.c
+++ qdujudger/src/rules/c_cpp_with_io.c
@@ -7,7 +7,7 @@
 #include "../runner.h"
 
 
-int c_cpp_seccomp_rules(struct config *_config) {
+int c_cpp_with_io_seccomp_rules(struct config *_config) {
     int syscalls_whitelist[] = {SCMP_SYS(read), SCMP_SYS(fstat),
                                 SCMP_SYS(mmap), SCMP_SYS(mprotect),
                                 SCMP_SYS(munmap), SCMP_SYS(uname),
